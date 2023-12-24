733bf633db12f98a25d64dab0cd1966a36444cbb
Emil <emilnovikov392@gmail.com>
2023-12-24 19:03:24 +0600

    add detapicker

--- components/v1/HomePage/HomePage.tsx
+++ components/v1/HomePage/HomePage.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import DatePickerComponent from "@/components/v1/DatePicker/DatePickerComponent";
-import Link from 'next/link';
-import Image from "next/image";
-import EventsList from "@/components/v1/HomePage/EventsList/EventsList";
 
 import { THomePageProps } from './types';
 import { HomePageContainer } from './styles'
 
 export const HomePage = ({ pageTitle }: THomePageProps) => {
     return <HomePageContainer>
-        <EventsList title="Совсем скоро"/>
-        <EventsList title="Концерты"/>
-        <EventsList title="Детям"/>
-        <EventsList title="Театр"/>
+        <h1>{pageTitle}</h1>
     </HomePageContainer>
 }
--- pages/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { SignInPage } from '@/components/v1/SignInPage/SignInPage';
-import { withAuthLayout } from '@/components/v1/layout/AuthLayout/AuthLayout';
-
-const SignInNextPage = () => {
- return <SignInPage />
-}
-
-export default withAuthLayout(SignInNextPage);
